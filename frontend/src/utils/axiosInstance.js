7771e3f4fdfc8e2c2aa5cb6b6979aab6fb452993
Tran Thanh <thanhhkh3@gmail.com>
2025-08-06 14:29:35 +0700

    update

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { setLoading } from './loading.js';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://saoviet-travel.onrender.com',
+    baseURL: process.env.REACT_APP_BASE_URL,
     timeout: 15000,
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
